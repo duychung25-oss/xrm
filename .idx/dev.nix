a3e95fe3fe205604e660743710fba3a4145cf7bc
duychung25-oss <duychung25@gmail.com>
2026-03-17 23:18:05 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,1 +1,48 @@
 
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+
+  packages = [
+    pkgs.qemu
+    pkgs.curl
+    pkgs.libguestfs
+    pkgs.apt
+    pkgs.openssl
+    pkgs.cdrkit
+    pkgs.cloud-utils
+    pkgs.openssh
+    pkgs.hostname
+    pkgs.cloud-init
+  ];
+
+  env = { };
+
+  idx = {
+    extensions = [ ];
+
+    workspace = {
+      onCreate = {
+      };
+
+onStart = {
+  command = ''
+    echo "🚀 Starting QEMU..."
+    chmod +x sh/run.sh
+
+    echo "🏁 Running setup script..."
+    until ./sh/run.sh ; do
+      echo "❌ Failed.  Retrying in 10 seconds..."
+      sleep 10
+    done
+
+    echo "✅ Script completed successfully."
+  '';
+};
+
+};
+
+    previews = {
+      enable = false;
+    };
+  };
+}
